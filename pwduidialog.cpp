a442a134c6a57db2f41b1cf0ad3bda01f6179229
Robert Hemstedt <sohrmann@web.de>
2011-10-01 21:05:21 +0200

    corrected regular expression

--- pwduidialog.cpp
+++ pwduidialog.cpp
@@ -12,15 +12,15 @@ PwduiDialog::PwduiDialog(QWidget *parent, int u, int p) :
     }
     lineEdit->setMaxLength(4);
     lineEdit_2->setMaxLength(4);
-    QRegExp regExp("[1-9]{1}[0-9]{2,4}");
+    QRegExp regExp("[1-9][0-9]{3}");
     lineEdit->setValidator(new QRegExpValidator(regExp, this));
-    QRegExp regExp2("[1-9]{1}[0-9]{2,4}");
+    QRegExp regExp2("[1-9][0-9]{3}");
     lineEdit_2->setValidator(new QRegExpValidator(regExp2,this));
 }
 
 void PwduiDialog::on_lineEdit_textChanged(const QString &arg1)
 {
-    if((lineEdit->hasAcceptableInput() && lineEdit_2->hasAcceptableInput()) && (! lineEdit->text().isEmpty()) && (! lineEdit_2->text().isEmpty()))
+    if((lineEdit->hasAcceptableInput()) && (lineEdit_2->hasAcceptableInput()) && (! lineEdit->text().isEmpty()) && (! lineEdit_2->text().isEmpty()))
     {
         this->buttonBox->setEnabled(true);
     }
@@ -29,7 +29,7 @@ void PwduiDialog::on_lineEdit_textChanged(const QString &arg1)
 
 void PwduiDialog::on_lineEdit_2_textChanged(const QString &arg1)
 {
-    if( (lineEdit->hasAcceptableInput() && lineEdit_2->hasAcceptableInput()))
+    if( (lineEdit->hasAcceptableInput()) && (lineEdit_2->hasAcceptableInput()))
     {
         this->buttonBox->setEnabled(true);
     } else buttonBox->setEnabled(false);
